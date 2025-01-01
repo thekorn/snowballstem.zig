aa630b290bef15c143195c9dfde2d863775466c1
Markus Korn <markus.korn@gmail.com>
2025-01-01 21:29:55 +0100

    WIP: Integrate snowballstem module into build process
    - Integrate new `snowballstem` module into the build process.
    - Implement `link_modul` function to handle compilation and linking for `snowballstem`.
    - Update build configuration to support new C source files associated with `snowballstem` module.

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-fn run_make_and_linkall(b: *std.Build, dep_snowball: *std.Build.Dependency, exe: *std.Build.Step.Compile, exe_unit_tests: *std.Build.Step.Compile) void {
+fn run_make_and_linkall(b: *std.Build, dep_snowball: *std.Build.Dependency, exe: *std.Build.Step.Compile, exe_unit_tests: *std.Build.Step.Compile, module_snowballstem: *std.Build.Module) void {
     var make_run = b.addSystemCommand(&.{"make"});
     make_run.setCwd(dep_snowball.path(""));
     //make_run.addArg("-v");
@@ -9,6 +9,7 @@ fn run_make_and_linkall(b: *std.Build, dep_snowball: *std.Build.Dependency, exe:
 
     link(b, exe, dep_snowball, make_run);
     link(b, exe_unit_tests, dep_snowball, make_run);
+    link_modul(b, module_snowballstem, dep_snowball, make_run);
 }
 
 fn link(b: *std.Build, cmp_stemp: *std.Build.Step.Compile, dep_snowball: *std.Build.Dependency, make_run: *std.Build.Step.Run) void {
@@ -90,6 +91,87 @@ fn link(b: *std.Build, cmp_stemp: *std.Build.Step.Compile, dep_snowball: *std.Bu
     cmp_stemp.step.dependOn(&make_run.step);
 }
 
+fn link_modul(b: *std.Build, cmp_stemp: *std.Build.Module, dep_snowball: *std.Build.Dependency, make_run: *std.Build.Step.Run) void {
+    _ = b;
+    _ = make_run;
+    //cmp_stemp.linkLibC();
+
+    cmp_stemp.addCSourceFiles(.{
+        .root = dep_snowball.path("libstemmer"),
+        .files = &.{"libstemmer.c"},
+    });
+
+    cmp_stemp.addCSourceFiles(.{
+        .root = dep_snowball.path("runtime"),
+        .files = &.{
+            "api.c",
+            "utilities.c",
+        },
+    });
+
+    cmp_stemp.addCSourceFiles(.{ .root = dep_snowball.path("src_c"), .files = &.{
+        "stem_ISO_8859_1_basque.c",
+        "stem_ISO_8859_1_catalan.c",
+        "stem_ISO_8859_1_danish.c",
+        "stem_ISO_8859_1_dutch.c",
+        "stem_ISO_8859_1_english.c",
+        "stem_ISO_8859_1_finnish.c",
+        "stem_ISO_8859_1_french.c",
+        "stem_ISO_8859_1_german.c",
+        "stem_ISO_8859_1_indonesian.c",
+        "stem_ISO_8859_1_irish.c",
+        "stem_ISO_8859_1_italian.c",
+        "stem_ISO_8859_1_norwegian.c",
+        "stem_ISO_8859_1_porter.c",
+        "stem_ISO_8859_1_portuguese.c",
+        "stem_ISO_8859_1_spanish.c",
+        "stem_ISO_8859_1_swedish.c",
+        "stem_ISO_8859_2_hungarian.c",
+        "stem_KOI8_R_russian.c",
+        "stem_UTF_8_arabic.c",
+        "stem_UTF_8_armenian.c",
+        "stem_UTF_8_basque.c",
+        "stem_UTF_8_catalan.c",
+        "stem_UTF_8_danish.c",
+        "stem_UTF_8_dutch.c",
+        "stem_UTF_8_estonian.c",
+        "stem_UTF_8_english.c",
+        "stem_UTF_8_finnish.c",
+        "stem_UTF_8_french.c",
+        "stem_UTF_8_german.c",
+        "stem_UTF_8_greek.c",
+        "stem_UTF_8_hindi.c",
+        "stem_UTF_8_hungarian.c",
+        "stem_UTF_8_indonesian.c",
+        "stem_UTF_8_irish.c",
+        "stem_UTF_8_italian.c",
+        "stem_UTF_8_lithuanian.c",
+        "stem_UTF_8_nepali.c",
+        "stem_UTF_8_norwegian.c",
+        "stem_UTF_8_porter.c",
+        "stem_UTF_8_portuguese.c",
+        "stem_UTF_8_romanian.c",
+        "stem_UTF_8_russian.c",
+        "stem_UTF_8_serbian.c",
+        "stem_UTF_8_spanish.c",
+        "stem_UTF_8_swedish.c",
+        "stem_UTF_8_tamil.c",
+        "stem_UTF_8_turkish.c",
+        "stem_UTF_8_yiddish.c",
+    } });
+
+    //cmp_stemp.installHeadersDirectory(dep_snowball.path(""), "include", .{
+    //    .include_extensions = &.{"libstemmer.h"},
+    //});
+
+    cmp_stemp.addIncludePath(dep_snowball.path("include"));
+    cmp_stemp.addIncludePath(dep_snowball.path("runtime"));
+    cmp_stemp.addIncludePath(dep_snowball.path("src_c"));
+
+    //cmp_stemp.step.dependOn(&make_run.step);
+    //cmp_stemp.linkLibrary(&make_run);
+}
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -130,5 +212,12 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 
-    run_make_and_linkall(b, dep_snowball, exe, exe_unit_tests);
+    const module_snowballstem = b.addModule("snowballstem", .{
+        .root_source_file = b.path("src/Stemmer.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    run_make_and_linkall(b, dep_snowball, exe, exe_unit_tests, module_snowballstem);
 }
