8bf9fb94990436ee03e40eb376054111a19b4f1b
Markus Korn <markus.korn@gmail.com>
2025-01-01 18:35:30 +0100

    Refactor build configuration in flake.nix
    - Simplify Nix build configuration by removing unnecessary dependencies.
    - Clean up environment settings by eliminating redundant configuration hooks and paths.

--- flake.nix
+++ flake.nix
@@ -23,24 +23,10 @@
           nativeBuildInputs = with pkgs; [
             zig-overlay.packages.${system}."master-2024-11-30"
             zls-master.packages.${system}.default
-            pkg-config
 
             lldb
             hyperfine
           ];
-          buildInputs = with pkgs; [
-              glfw
-          ];
-
-          shellHook = ''
-            # We unset some NIX environment variables that might interfere with the zig
-            # compiler.
-            # Issue: https://github.com/ziglang/zig/issues/18998
-            unset NIX_CFLAGS_COMPILE
-            #unset NIX_LDFLAGS
-          '';
-
-          PKG_CONFIG_PATH = "${pkgs.glfw}/lib/pkgconfig";
         };
       }
     );
